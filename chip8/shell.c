d3ae65bce56891c73585e76ba06c39e5dffc888c
Florin Salasan <florinsalasan@gmail.com>
2024-03-25 17:23:56 -0400

    added clear screen function to be used later

--- chip8/shell.c
+++ chip8/shell.c
@@ -1,5 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+const int SCREEN_HEIGHT = 32;
+const int SCREEN_WIDTH = 64;
+
+void clear_screen(bool[SCREEN_HEIGHT][SCREEN_WIDTH]);
 
 int main () {
 
@@ -7,3 +13,14 @@ int main () {
 
 }
 
+void clear_screen (bool display[SCREEN_HEIGHT][SCREEN_WIDTH]) {
+
+    int i, j;
+    for (i = 0; i < SCREEN_HEIGHT; i++) {
+        for (j = 0; j < SCREEN_WIDTH; j++) {
+            display[j][i] = false;
+        }
+    }
+    printf("clearing");
+
+}
