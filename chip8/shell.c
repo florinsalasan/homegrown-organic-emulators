c6ff7e3c622174175f22c57c13acc2708a304c10
Florin Salasan <florinsalasan@gmail.com>
2024-03-27 16:58:11 -0400

    small additions to the chip8 emulator

--- chip8/shell.c
+++ chip8/shell.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-const int SCREEN_HEIGHT = 32;
-const int SCREEN_WIDTH = 64;
+#define SCREEN_HEIGHT 32
+#define SCREEN_WIDTH 64
 
 void clear_screen(bool[SCREEN_HEIGHT][SCREEN_WIDTH]);
 
@@ -27,8 +27,21 @@ unsigned char fontset[80] = {
     0xF0, 0x80, 0xF0, 0x80, 0x80   // F
 };
 
+// '4kb' of memory
 unsigned char memory[4096] = {0};
-bool display[64][32] = {false};
+
+// display of 32x64
+bool display[SCREEN_WIDTH][SCREEN_HEIGHT] = {false};
+
+// Use this for the call stack, early ones apparently had space for two
+// instructions, I went a bit overboard and have space for 32 16-bit addresses
+unsigned short stack[32] = {0};
+
+// The delay and sound timer registers:
+unsigned char delay_timer = 0;
+unsigned char sound_timer = 0;
+
+// implement the keypad somehow:
 
 int main () {
     // Run the instruction loop in here
