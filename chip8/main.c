050318728a77be73def807eb2b59d9e26d8c3569
Florin Salasan <florinsalasan@gmail.com>
2024-04-23 18:32:03 -0400

    reworked CMakeLists file so that the SDL framework is being used, also removed the directory with the rust project and am reworking all of the chip8 files so that they are housed in one file

--- chip8/main.c
+++ chip8/main.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
 #include <stdbool.h>
-#include "../include/sdl_layer.h"
 
-#include <SDL2/SDL.h>
+#include <SDL.h>
+
+// Define screen dimensions
+#define SCREEN_WIDTH    64
+#define SCREEN_HEIGHT   32
 
 SDL_Window* screen;
-
 SDL_Renderer* renderer;
 
 SDL_Scancode keymappings[16] = {
@@ -14,26 +17,12 @@ SDL_Scancode keymappings[16] = {
     SDL_SCANCODE_Z, SDL_SCANCODE_X, SDL_SCANCODE_C, SDL_SCANCODE_V,
 };
 
-bool should_quit = false;
+int should_quit = 0;
 
-// Display initialization
-void init_display(void) {
+void init_sdl_display(void) {
     SDL_Init(SDL_INIT_VIDEO);
-
     screen = SDL_CreateWindow("CHIP-8", SDL_WINDOWPOS_CENTERED,
-                              SDL_WINDOWPOS_CENTERED, 64 * 8, 32 * 8, 0);
+                             SDL_WINDOWPOS_CENTERED, 64*8, 32*8, 0);
     renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
 }
 
-// draw on sdl display
-void draw(unsigned char* display) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-
-    // clear the display
-    SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-    // Iterate through the display (64*32)
-
-}
