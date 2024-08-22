0f6634942e00bac18e207a820aababd7aabc9baf
Florin Salasan <florinsalasan@gmail.com>
2024-08-22 10:15:14 -0400

    added the first opcode from a guide and wrote two tests for it

--- nes/src/main.rs
+++ nes/src/main.rs
@@ -13,11 +13,69 @@ impl CPU {
         }
     }
 
+    // The main CPU loop is:
+    // Fetch next instruction from memory,
+    // Decode the instruction,
+    // Execute the instruction,
+    // repeat;
+
     pub fn interpret(&mut self, program: Vec<u8>) {
-        todo!("Opcode things")
+        self.program_counter = 0;
+
+        loop {
+            let opcode = program[self.program_counter as usize];
+            self.program_counter += 1;
+
+            match opcode {
+                0x00 => { 
+                    return;
+                }
+                0xA9 => {
+                    // 0xA9 LDA (Load accumulator) in immediate addressing mode,
+                    // 2 bytes, 2 cycles according to the reference table
+                    let param = program[self.program_counter as usize];
+                    self.program_counter += 1;
+                    self.register_a = param;
+
+                    if self.register_a == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    if self.register_a & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                }
+                _ => todo!("Build out the massive switch statement for opcodes")
+            }
+        }
     }
 }
 
 fn main() {
     
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_0xa9_lda_immediate_load_data() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        assert_eq!(cpu.register_a, 0x05);
+        assert!(cpu.status & 0b0000_0010 == 0b00);
+        assert!(cpu.status & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
 }
