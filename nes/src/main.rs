a687fd25da288f165d081e853d76c435e9004edf
Florin Salasan <florinsalasan@gmail.com>
2024-08-25 11:33:11 -0400

    added in a reset method and used the u16 methods for reading and writing from/to memory

--- nes/src/main.rs
+++ nes/src/main.rs
@@ -68,14 +68,22 @@ impl CPU {
     }
 
     pub fn load_and_run(&mut self, program: Vec<u8>) {
-        let _cloned = program.clone();
         self.load(program);
+        self.reset();
         self.run();
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+
+        self.program_counter = self.mem_read_u16(0xFFFC);
     }
 
     pub fn load(&mut self, program: Vec<u8>) {
         self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
-        self.program_counter = 0x8000;
+        self.mem_write_u16(0xFFFC, 0x8000)
     }
 
     // for mem_read_u16 and mem_write_u16 double check that this isn't breaking anything
